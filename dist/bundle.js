d01307245d361d5495cad90e94e9b2b424482b01
Max Shymchuk <octorix@mail.ru>
2019-11-21 01:52:04 +0300

    fix: animation end state fix

--- dist/bundle.js
+++ dist/bundle.js
@@ -817,17 +817,21 @@
                   if (!props.time) props.time = 500;
                   if (!props.animeType) props.animeType = 'ease-in-out';
                   if (!props.dir) props.dir = AnimeDir.Normal;
+                  if (!props.count) props.count = 1;
+                  if (!props.fillMode) props.fillMode = 'forwards';
                   object.style.animation = [
                     props.name,
                     ''.concat(props.time / 1000, 's'),
                     props.dir,
-                    props.animeType
+                    props.animeType,
+                    props.count,
+                    props.fillMode
                   ].join(' ');
                   object.style.pointerEvents = 'none';
                   setTimeout(function() {
+                    if (callback) callback();
                     object.style.animation = '';
                     object.style.pointerEvents = 'auto';
-                    if (callback) callback();
                   }, props.time);
                 }
               }
--- src/classes/Animation.js
+++ src/classes/Animation.js
@@ -9,12 +9,21 @@ export class Animation {
     if (!props.time) props.time = 500;
     if (!props.animeType) props.animeType = 'ease-in-out';
     if (!props.dir) props.dir = AnimeDir.Normal;
-    object.style.animation = [props.name, `${props.time / 1000}s`, props.dir, props.animeType].join(' ');
+    if (!props.count) props.count = 1;
+    if (!props.fillMode) props.fillMode = 'forwards';
+    object.style.animation = [
+      props.name,
+      `${props.time / 1000}s`,
+      props.dir,
+      props.animeType,
+      props.count,
+      props.fillMode
+    ].join(' ');
     object.style.pointerEvents = 'none';
     setTimeout(() => {
+      if (callback) callback();
       object.style.animation = '';
       object.style.pointerEvents = 'auto';
-      if (callback) callback();
     }, props.time);
   }
 }
